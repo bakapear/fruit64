c6ce977eb50910e153623e6c21b8cbb27ba1ce5c
bakapear <proxybomb@gmail.com>
2022-12-29 12:52:36 +0100

    add tvmode fn

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <libdragon.h>
 
+void setTVMode(tv_type_t mode) {
+    *(uint32_t *)0x80000300 = mode;
+}
+
 int main(void) {
+    setTVMode(TV_PAL);
+
     console_init();
     printf("Hello world!\n");
 }

d10df6d069b884ef3c8bb1cb4299890f11adfc12
bakapear <proxybomb@gmail.com>
2023-01-03 12:55:58 +0100

    use cen64 from path

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
 docker run --rm -v "$(pwd):/build" ghcr.io/ariahiro64/altra64:master make clean
 docker run --rm -v "$(pwd):/build" ghcr.io/ariahiro64/altra64:master make
 mv "out/fruit64.z64" "out/OS64P.v64"
-B:\programs\cen64\cen64.bat "out/OS64P.v64"
+cen64 "out/OS64P.v64"

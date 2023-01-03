353614178e1ca8da21b1d083f9654237cd8e36e9
bakapear <proxybomb@gmail.com>
2023-01-03 12:53:51 +0100

    add tvmode & sdspeed fns

--- src/ed64.h
+++ src/ed64.h
@@ -1,6 +1,17 @@
 #ifndef _ED64_H
 #define _ED64_H
 
+typedef enum {
+    TV_PAL = 0,
+    TV_NTSC = 1,
+    TV_MPAL = 2
+} tv_mode_t;
+
+typedef enum {
+    SD_25MHZ = 0,
+    SD_50MHZ = 1
+} sd_speed_t;
+
 int ed64init();
 void ed64close();
 
--- src/main.c
+++ src/main.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 
 #include "ed64.h"
+#include "gfx.h"
+
+void setTVMode(tv_mode_t mode) {
+    *(uint32_t *)0x80000300 = mode;
+}
+
+void setSDSpeed(sd_speed_t speed) {
+    switch (speed) {
+        case SD_25MHZ: return bi_speed25();
+        case SD_50MHZ: return bi_speed50();
+    }
+}
 
 int main() {
     int fat = ed64init();
 
+    setTVMode(TV_PAL);
+    setSDSpeed(SD_50MHZ);
     display_init(RESOLUTION_320x240, DEPTH_32_BPP, 3, GAMMA_NONE, ANTIALIAS_RESAMPLE);
 
     gfx_lock();
